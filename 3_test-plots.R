37421799f7cc67ee6b5f66623f62da32bf6a02a2
oDNAudio <lukas.vashold@gmx.at>
2019-01-13 14:07:40 +0100

    fixed plot list generation

--- 3_test-plots.R
+++ 3_test-plots.R
@@ -42,23 +42,23 @@ poly_plot_gdp_pc <- na.omit(left_join(poly_ggplot, gdp_pc[ , c(1, 38:ncol(gdp_pc
 gdp_pc_plot_list <- list()
 plot_names <- names(poly_plot_gdp_pc)[8:ncol(poly_plot_gdp_pc)]
 
-for(i in plot_names){
- gdp_pc_plot_list[[i]] <- ggplot() + 
-   geom_polygon(data = poly_plot_gdp_pc, 
-                aes(fill = poly_plot_gdp_pc[ , which(names(poly_plot_gdp_pc) == i)], 
-                    x = long, y = lat, group = group)) + 
-   geom_path(data = poly_plot_gdp_pc, aes(x = long, y = lat, group = group), 
-             color = "black", size = 0.05) + 
-   labs(x = NULL, y = NULL) + 
-   theme(axis.line = element_blank(), 
-         axis.text.x=element_blank(), axis.text.y = element_blank(), 
-         axis.ticks = element_blank(), 
-         axis.title.x = element_blank(), axis.title.y = element_blank()) + 
-   coord_equal() + 
-   scale_fill_viridis(option = "plasma", direction = -1, discrete = TRUE) + 
-   theme(legend.position = "none", legend.justification = c(0,1), 
-         legend.background = element_rect(fill="transparent"), 
-         legend.title = element_blank())
+for(i in 1:length(plot_names)){
+  gdp_pc_plot_list[[plot_names[i]]]  <- ggplot() + 
+    geom_polygon(data = poly_plot_gdp_pc, 
+                 aes_string(fill = plot_names[i], 
+                            x = "long", y = "lat", group = "group")) + 
+    geom_path(data = poly_plot_gdp_pc, aes(x = long, y = lat, group = group), 
+              color = "black", size = 0.05) + 
+    labs(x = NULL, y = NULL) + 
+    theme(axis.line = element_blank(), 
+          axis.text.x=element_blank(), axis.text.y = element_blank(), 
+          axis.ticks = element_blank(), 
+          axis.title.x = element_blank(), axis.title.y = element_blank()) + 
+    coord_equal() + 
+    scale_fill_viridis(option = "plasma", direction = -1, discrete = TRUE) + 
+    theme(legend.position = "none", legend.justification = c(0,1), 
+          legend.background = element_rect(fill="transparent"), 
+          legend.title = element_blank())
 }
 
 
@@ -92,11 +92,11 @@ poly_plot_gdp_gr <- na.omit(left_join(poly_ggplot, gdp_gr[ , c(1, 10:ncol(gdp_gr
 gdp_gr_plot_list <- list()
 plot_names <- names(poly_plot_gdp_gr)[8:ncol(poly_plot_gdp_gr)]
 
-for(i in plot_names){
-  gdp_gr_plot_list[[i]] <- ggplot() + 
+for(i in 1:length(plot_names)){
+  gdp_gr_plot_list[[plot_names[i]]]  <- ggplot() + 
     geom_polygon(data = poly_plot_gdp_gr, 
-                 aes(fill = poly_plot_gdp_gr[ , which(names(poly_plot_gdp_gr) == i)], 
-                     x = long, y = lat, group = group)) + 
+                 aes_string(fill = plot_names[i], 
+                            x = "long", y = "lat", group = "group")) + 
     geom_path(data = poly_plot_gdp_gr, aes(x = long, y = lat, group = group), 
               color = "black", size = 0.05) + 
     labs(x = NULL, y = NULL) + 
@@ -112,12 +112,122 @@ for(i in plot_names){
 }
 
 
+#####
+# GVA plots in case necessary
+
+# gva_pc <- depvar_list$gva_pc
+# 
+# quant_gva_pc_00 <- quantile(gva_pc$GVA_pc_2000, probs = seq(0, 1, by = 1/6))
+# quant_gva_pc_07 <- quantile(gva_pc$GVA_pc_2007, probs = seq(0, 1, by = 1/6))
+# quant_gva_pc_08 <- quantile(gva_pc$GVA_pc_2008, probs = seq(0, 1, by = 1/6))
+# quant_gva_pc_15 <- quantile(gva_pc$GVA_pc_2015, probs = seq(0, 1, by = 1/6))
+# 
+# equal_gva_pc_00 <- seq(min(gva_pc$GVA_pc_2000), max(gva_pc$GVA_pc_2000), 
+#                        by = (max(gva_pc$GVA_pc_2000) - min(gva_pc$GVA_pc_2000)))
+# equal_gva_pc_07 <- seq(min(gva_pc$GVA_pc_2007), max(gva_pc$GVA_pc_2007), 
+#                        by = (max(gva_pc$GVA_pc_2007) - min(gva_pc$GVA_pc_2007)))
+# equal_gva_pc_08 <- seq(min(gva_pc$GVA_pc_2008), max(gva_pc$GVA_pc_2008), 
+#                        by = (max(gva_pc$GVA_pc_2008) - min(gva_pc$GVA_pc_2008)))
+# equal_gva_pc_15 <- seq(min(gva_pc$GVA_pc_2015), max(gva_pc$GVA_pc_2015), 
+#                        by = (max(gva_pc$GVA_pc_2015) - min(gva_pc$GVA_pc_2015)))
+# 
+# gva_pc$quant_gva_pc_00 <- cut(gva_pc$GVA_pc_2000, breaks = quant_gva_pc_00, include_lowest = T)
+# gva_pc$quant_gva_pc_07 <- cut(gva_pc$GVA_pc_2007, breaks = quant_gva_pc_07, include_lowest = T)
+# gva_pc$quant_gva_pc_08 <- cut(gva_pc$GVA_pc_2008, breaks = quant_gva_pc_08, include_lowest = T)
+# gva_pc$quant_gva_pc_15 <- cut(gva_pc$GVA_pc_2015, breaks = quant_gva_pc_15, include_lowest = T)
+# 
+# gva_pc$equal_gva_pc_00 <- cut(gva_pc$GVA_pc_2000, breaks = equal_gva_pc_00, include_lowest = T)
+# gva_pc$equal_gva_pc_07 <- cut(gva_pc$GVA_pc_2007, breaks = equal_gva_pc_07, include_lowest = T)
+# gva_pc$equal_gva_pc_08 <- cut(gva_pc$GVA_pc_2008, breaks = equal_gva_pc_08, include_lowest = T)
+# gva_pc$equal_gva_pc_15 <- cut(gva_pc$GVA_pc_2015, breaks = equal_gva_pc_15, include_lowest = T)
+# 
+# 
+# poly_plot_gva_pc <- na.omit(left_join(poly_ggplot, gva_pc[ , c(1, 38:ncol(gva_pc))], by = c("id" = "NUTS_ID")))
+# 
+# 
+# gva_pc_plot_list <- list()
+# plot_names <- names(poly_plot_gva_pc)[8:ncol(poly_plot_gva_pc)]
+# 
+# for(i in 1:length(plot_names)){
+#   gva_pc_plot_list[[plot_names[i]]]  <- ggplot() + 
+#     geom_polygon(data = poly_plot_gva_pc, 
+#                  aes_string(fill = plot_names[i], 
+#                             x = "long", y = "lat", group = "group")) + 
+#     geom_path(data = poly_plot_gva_pc, aes(x = long, y = lat, group = group), 
+#               color = "black", size = 0.05) + 
+#     labs(x = NULL, y = NULL) + 
+#     theme(axis.line = element_blank(), 
+#           axis.text.x=element_blank(), axis.text.y = element_blank(), 
+#           axis.ticks = element_blank(), 
+#           axis.title.x = element_blank(), axis.title.y = element_blank()) + 
+#     coord_equal() + 
+#     scale_fill_viridis(option = "plasma", direction = -1, discrete = TRUE) + 
+#     theme(legend.position = "none", legend.justification = c(0,1), 
+#           legend.background = element_rect(fill="transparent"), 
+#           legend.title = element_blank())
+# }
+# 
+# 
+# 
+# 
+# gva_gr <- depvar_list$gva_pc_gr
+# 
+# quant_gva_gr_0015 <- quantile(gva_gr$GVA_gr_0015, probs = seq(0, 1, by = 1/6))
+# quant_gva_gr_0007 <- quantile(gva_gr$GVA_gr_0007, probs = seq(0, 1, by = 1/6))
+# quant_gva_gr_0815 <- quantile(gva_gr$GVA_gr_0815, probs = seq(0, 1, by = 1/6))
+# 
+# equal_gva_gr_0015 <- seq(min(gva_gr$GVA_gr_0015), max(gva_gr$GVA_gr_0015), 
+#                          by = (max(gva_gr$GVA_gr_0015) - min(gva_gr$GVA_gr_0015)))
+# equal_gva_gr_0007 <- seq(min(gva_gr$GVA_gr_0007), max(gva_gr$GVA_gr_0007), 
+#                          by = (max(gva_gr$GVA_gr_0007) - min(gva_gr$GVA_gr_0007)))
+# equal_gva_gr_0815 <- seq(min(gva_gr$GVA_gr_0815), max(gva_gr$GVA_gr_0815), 
+#                          by = (max(gva_gr$GVA_gr_0815) - min(gva_gr$GVA_gr_0815)))
+# 
+# gva_gr$quant_gva_gr_0015 <- cut(gva_gr$GVA_gr_0015, breaks = quant_gva_gr_0015, include_lowest = T)
+# gva_gr$quant_gva_gr_0007 <- cut(gva_gr$GVA_gr_0007, breaks = quant_gva_gr_0007, include_lowest = T)
+# gva_gr$quant_gva_gr_0815 <- cut(gva_gr$GVA_gr_0815, breaks = quant_gva_gr_0815, include_lowest = T)
+# 
+# gva_gr$equal_gva_gr_0015 <- cut(gva_gr$GVA_gr_0015, breaks = equal_gva_gr_0015, include_lowest = T)
+# gva_gr$equal_gva_gr_0007 <- cut(gva_gr$GVA_gr_0007, breaks = equal_gva_gr_0007, include_lowest = T)
+# gva_gr$equal_gva_gr_0815 <- cut(gva_gr$GVA_gr_0815, breaks = equal_gva_gr_0815, include_lowest = T)
+# 
+# 
+# poly_plot_gva_gr <- na.omit(left_join(poly_ggplot, gva_gr[ , c(1, 10:ncol(gva_gr))], by = c("id" = "NUTS_ID")))
+# 
+# 
+# gva_gr_plot_list <- list()
+# plot_names <- names(poly_plot_gva_gr)[8:ncol(poly_plot_gva_gr)]
+# 
+# for(i in 1:length(plot_names)){
+#   gva_gr_plot_list[[plot_names[i]]]  <- ggplot() + 
+#     geom_polygon(data = poly_plot_gva_gr, 
+#                  aes_string(fill = plot_names[i], 
+#                             x = "long", y = "lat", group = "group")) + 
+#     geom_path(data = poly_plot_gva_gr, aes(x = long, y = lat, group = group), 
+#               color = "black", size = 0.05) + 
+#     labs(x = NULL, y = NULL) + 
+#     theme(axis.line = element_blank(), 
+#           axis.text.x=element_blank(), axis.text.y = element_blank(), 
+#           axis.ticks = element_blank(), 
+#           axis.title.x = element_blank(), axis.title.y = element_blank()) + 
+#     coord_equal() + 
+#     scale_fill_viridis(option = "plasma", direction = -1, discrete = TRUE) + 
+#     theme(legend.position = "none", legend.justification = c(0,1), 
+#           legend.background = element_rect(fill="transparent"), 
+#           legend.title = element_blank())
+# }
+# 
+# save(poly_plot_gdp_pc, gdp_pc_plot_list, 
+#      poly_plot_gdp_gr, gdp_gr_plot_list, 
+#      poly_plot_gva_pc, gva_pc_plot_list, 
+#      poly_plot_gva_gr, gva_gr_plot_list, 
+#      file = "./data/plots.Rda")
+#####
+
 
 
 save(poly_plot_gdp_pc, gdp_pc_plot_list, 
      poly_plot_gdp_gr, gdp_gr_plot_list, 
      file = "./data/plots.Rda")
-
-load("./data/plots.Rda")
 
 
